a2efa214e568eae53334a0c1f8d51509a1087eba
Andrew <kvanonavk@gmail.com>
2018-02-24 23:50:09 +0200

    Initial commit

---
+++ Assets/Scripts/SoundButtonScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+/// <summary>
+/// Changes volume status of a specified audio source.
+/// </summary>
+public class SoundButtonScript : MonoBehaviour {
+
+    public AudioSource audioSource;
+
+    public Sprite soundOn, soundOff;
+
+    private Image curImage;
+
+    private void Awake()
+    {
+        curImage = GetComponent<Image>();
+    }
+
+    public void ChangeVolumeState()
+    {
+        audioSource.mute = !audioSource.mute;
+
+        if (audioSource.mute) curImage.sprite = soundOff;
+        else curImage.sprite = soundOn;
+    }
+}
